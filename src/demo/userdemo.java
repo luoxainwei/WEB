6ad21d7f584bd0473af44f619e2a5cd7d183d98c
罗贤伟 <37461913+luoxainwei@users.noreply.github.com>
2020-07-02 17:27:39 +0800

    Update userdemo.java

--- src/demo/userdemo.java
+++ src/demo/userdemo.java
@@ -1,14 +1,20 @@
 package demo;
 import java.util.HashMap;
+
 public class userdemo {
-    static HashMap<String, String> users = new HashMap<>();
+    static HashMap<String, String> users = new HashMap<>();//内存数据库
+    static{
+        users.put("admin","123456");
+        users.put("tom","123");
+        users.put("cat","456");
+    }
     public static boolean register(String name, String pass) {
         if (users.containsKey(name) || name.trim().isEmpty()) {
             System.err.println("注册失败");
             return false;
         }
         users.put(name, pass);
-        System.err.println("用户不存在");
+        System.err.println("注册成功!!");
         return true;
     }
 
@@ -21,29 +27,17 @@ public class userdemo {
             System.err.println("密码错误");
             return false;
         }
-        System.err.println("登陆成功");
+        System.err.println("登录成功");
         return true;
     }
 
     private static boolean updatePassword(String name, String oldPass, String newPass) {
         if (!login(name, oldPass)) {
-            System.err.println("旧密码错误，修改失败");
+            System.err.println("旧密码错误,修改失败");
             return false;
+
         }
         users.put(name, newPass);
-        {
-            System.err.println("修改密码成功，请登录");
-            return true;
-        }
+        System.err.println("修改密码成功，请使用新密码登录");
+        return true;
     }
-
-    public static void main(String[] args) {
-        register("tom", "123456");
-        register("cat", "666666");
-        System.out.println(login("tom", "123456"));
-        System.out.println(login("cat", "666666"));
-        System.out.println(updatePassword("tom", "123456", "654321"));
-        System.out.println(login("tom", "123456"));
-        System.out.println(login("tom", "654321"));
-    }
-}
